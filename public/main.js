c9eb7c5e450e0cfe912d57500650d5686833f28c
Elro1118 <silvana.rojas8119@gmail.com>
2019-02-12 15:30:14 -0500

    weatherAPIFirstFase

--- public/main.js
+++ public/main.js
@@ -1,7 +1,70 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+let currentWeather = {}
+let city = ''
+const isNumber = str => {
+  return !isNaN(parseFloat(str)) && !isNaN(str - 0)
+}
+
+const getWeather = () => {
+  let cityValue = document.querySelector('.weather-search-text').value
+  if (cityValue.length === 0) {
+    document.querySelector('.weather-city-label').textContent =
+      'The textbox is empty.'
+  } else {
+    console.log(isNumber(cityValue))
+    if (isNumber(cityValue)) {
+      getWeatherZip()
+    } else {
+      getWeatherName()
+    }
   }
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const getWeatherName = () => {
+  city = document.querySelector('.weather-search-text').value
+
+  console.log(isNumber(city))
+  // send the request
+
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?appid=59c6f243bf44bdca855e2e8f1c663981&q=' +
+      city
+  )
+    // getting the response back
+    .then(resp => {
+      return resp.json()
+    })
+    // opening the response, joke is the actual data that we want
+    .then(weather => {
+      console.log(weather)
+      currentWeather = weather
+      document.querySelector('.weather-city-label').textContent =
+        'The Weather in ' + city + ' ' + weather.weather[0].main
+      console.log(weather.weather[0].main)
+    })
+}
+
+const getWeatherZip = () => {
+  city = document.querySelector('.weather-search-text').value
+
+  console.log(isNumber(city))
+  // send the request
+
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?appid=59c6f243bf44bdca855e2e8f1c663981&zip=' +
+      city +
+      ',us'
+  )
+    // getting the response back
+    .then(resp => {
+      return resp.json()
+    })
+    // opening the response, joke is the actual data that we want
+    .then(weather => {
+      console.log(weather)
+      currentWeather = weather
+      document.querySelector('.weather-city-label').textContent =
+        'The Weather in ' + weather.name + ' ' + weather.weather[0].main
+      console.log(weather.weather[0].main)
+    })
+}
+document.querySelector('.search-button').addEventListener('click', getWeather)
