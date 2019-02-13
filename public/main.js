105fd6547e25992558bc6a323e30d19f67fb1fd2
Elro1118 <silvana.rojas8119@gmail.com>
2019-02-12 20:37:42 -0500

    ExplorerMode

--- public/main.js
+++ public/main.js
@@ -1,10 +1,9 @@
-let currentWeather = {}
-let city = ''
 const isNumber = str => {
   return !isNaN(parseFloat(str)) && !isNaN(str - 0)
 }
 
 const getWeather = () => {
+  let url = ''
   let cityValue = document.querySelector('.weather-search-text').value
   if (cityValue.length === 0) {
     document.querySelector('.weather-city-label').textContent =
@@ -12,23 +11,29 @@ const getWeather = () => {
   } else {
     console.log(isNumber(cityValue))
     if (isNumber(cityValue)) {
-      getWeatherZip()
+      console.log(cityValue)
+      url =
+        'https://api.openweathermap.org/data/2.5/weather?appid=59c6f243bf44bdca855e2e8f1c663981&zip=' +
+        cityValue +
+        ',us'
+      // `https://api.openweathermap.org/data/2.5/weather?appid=59c6f243bf44bdca855e2e8f1c663981&zip=
+      // ${cityValue},us&units=imperial`
+      getWeatherName(url)
+      // getWeatherZip()
     } else {
-      getWeatherName()
+      url =
+        // 'https://api.openweathermap.org/data/2.5/weather?appid=59c6f243bf44bdca855e2e8f1c663981&q=' +
+        // cityValue
+        `https://api.openweathermap.org/data/2.5/weather?appid=59c6f243bf44bdca855e2e8f1c663981&q=${cityValue}&units=imperial`
+      getWeatherName(url)
     }
   }
 }
 
-const getWeatherName = () => {
-  city = document.querySelector('.weather-search-text').value
+const getWeatherName = url => {
+  let currentWeather = {}
 
-  console.log(isNumber(city))
-  // send the request
-
-  fetch(
-    'https://api.openweathermap.org/data/2.5/weather?appid=59c6f243bf44bdca855e2e8f1c663981&q=' +
-      city
-  )
+  fetch(url)
     // getting the response back
     .then(resp => {
       return resp.json()
@@ -38,33 +43,12 @@ const getWeatherName = () => {
       console.log(weather)
       currentWeather = weather
       document.querySelector('.weather-city-label').textContent =
-        'The Weather in ' + city + ' ' + weather.weather[0].main
-      console.log(weather.weather[0].main)
+        'The Weather in ' +
+        currentWeather.name +
+        ' is ' +
+        currentWeather.weather[0].main
+      console.log(currentWeather.weather[0].main)
     })
 }
 
-const getWeatherZip = () => {
-  city = document.querySelector('.weather-search-text').value
-
-  console.log(isNumber(city))
-  // send the request
-
-  fetch(
-    'https://api.openweathermap.org/data/2.5/weather?appid=59c6f243bf44bdca855e2e8f1c663981&zip=' +
-      city +
-      ',us'
-  )
-    // getting the response back
-    .then(resp => {
-      return resp.json()
-    })
-    // opening the response, joke is the actual data that we want
-    .then(weather => {
-      console.log(weather)
-      currentWeather = weather
-      document.querySelector('.weather-city-label').textContent =
-        'The Weather in ' + weather.name + ' ' + weather.weather[0].main
-      console.log(weather.weather[0].main)
-    })
-}
 document.querySelector('.search-button').addEventListener('click', getWeather)
